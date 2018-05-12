fbefa8352f130df5ae4ee2c660e97d18d52ea5a7
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-12 19:00:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,8 +27,8 @@ class TicTacToe
     user_input.to_i - 1
   end
 
-  def move(board, index, current_player)
-    board[index] = current_player
+  def move(index, current_player = "X")
+    @board[index] = current_player
   end
 
   def position_taken?(board, index)
